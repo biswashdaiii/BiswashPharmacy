8db122ff02caa5d22783f975e24e034f6820099e
Biswash Ghimire <biswashghimire97@gmail.com>
2026-01-29 03:55:06 +0545

    profile done

--- web_frontend/src/pages/MyProfile.jsx
+++ web_frontend/src/pages/MyProfile.jsx
@@ -441,7 +441,7 @@ const MyProfile = () => {
         </div>
       )}
 
-      {/* QR Code Modal for 2FA Setup */}
+
       {showQRModal && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50">
           <div className="bg-white rounded-lg p-6 w-full max-w-md">
